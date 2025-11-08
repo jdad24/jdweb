3101880e2129bc45770cb6eea2631089fb5b8c77
Joshua Dadson <joshua.dadson@ibm.com>
2025-11-08 12:42:04 -0600

    experience update

--- src/pages/experience/index.jsx
+++ src/pages/experience/index.jsx
@@ -1,16 +1,37 @@
 import './index.scss'
 
 const Experience = () => {
+    const stackList = ['React.js', 'Node.js', 'Swift', 'JavaScript', 'Python']
+    const renderStackItems = (items) => {
+        return (
+            <div className='stack-container'>
+                {items.map(item =>
+                    <div className='stack-item'>{item}</div>
+                )}
+            </div>
+        )
+    }
     return (
         <div className="experience-container">
             <div className="title">Experience</div>
             <div className='subtitle'>6+ years building scalable web and iOS applications </div>
-            <div className='tech-stack-container'>Tech Stack</div>
-            <div className='job-history-container'>
-                <div className='job'>Lead ReactJS UI Developer</div>
-                <div className='job'>Frontend Developer</div>
-                <div className='job'>Cloud Engineer</div>
-                <div className='job'>IT Specialist</div>
+            <div className='section-1'>
+                <div className='stack-title'>Primary Stack</div>
+                {renderStackItems(stackList)}
+            </div>
+            <div className='section-2'>
+                <div className='job'>
+                    <div className='job-title'>Lead ReactJS UI Developer</div>
+                </div>
+                <div className='job'>
+                    <div className='job-title'>Frontend Developer</div>
+                </div>
+                <div className='job'>
+                    <div className='job-title'>Cloud Engineer</div>
+                </div>
+                <div className='job'>
+                    <div className='job-title'>IT Specialist</div>
+                </div>
             </div>
         </div>
     )
